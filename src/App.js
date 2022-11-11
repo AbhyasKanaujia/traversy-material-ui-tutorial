1a02a88920ebce60e4b36fa420acb0e8e0ea78f1
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 22:03:11 +0530

    Add Checkbox and label

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import Save from "@mui/icons-material/Save";
 import Delete from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { useState } from "react";
+
+const CheckBoxExample = () => {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          ox
+          checked={checked}
+          icon={<Save />}
+          checkedIcon={<Delete />}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckBoxExample />
         <ButtonGroup>
           <Button
             startIcon={<Save />}
